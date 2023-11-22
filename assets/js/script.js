94a545bc80f73c314b829f26fc34146c5b1e3dd2
DavidePeloso-dev <davide.peloso.e@gmail.com>
2023-11-22 22:11:06 +0100

    Complete program

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,20 @@ const age = document.getElementById('age')
 
 const cost = 0.21;
 
+const tiket_wrapper = document.querySelector('.tiket_wrapper').classList;
+
+const modal = document.querySelector('.bg_modal').classList;
+
+
 generate_tiket.addEventListener('click', function() {
 
     if(kilometers.value == "" || age.value == "" || full_name.value == "") {
-        alert('Complete all forms')
-    }
+        modal.remove('invisible');
+        const close = document.querySelector('.close');
+        close.addEventListener('click', function(){
+            modal.add('invisible')
+        })
+    } else{
     
 
     const tiket_price = kilometers.value * cost;
@@ -22,14 +31,34 @@ generate_tiket.addEventListener('click', function() {
  
     if(Number(age.value) < 18){
         tiket_discount = tiket_price * 20 / 100
+        document.querySelector('.tiket_tipe').innerHTML = 'Discounted'
+
     } else if(Number(age.value) > 65){
         tiket_discount = tiket_price * 40 / 100
+        document.querySelector('.tiket_tipe').innerHTML = 'Discounted'
+
     } else {
         tiket_discount = 0.00
+        document.querySelector('.tiket_tipe').innerHTML = 'Standard'
+
     };
 
     const total_price = (tiket_price - tiket_discount).toFixed(2);
     console.log(total_price);
+
+    document.querySelector('.full_name').innerHTML = full_name.value;
+
+    document.querySelector('.wagon').innerHTML = Math.floor(Math.random() * 10 + 1);
+
+    document.querySelector('.tiket_price').innerHTML = total_price;
+
+    document.querySelector('.cp_code').innerHTML = Math.floor(Math.random() * (100000 -10000) + 1) + 10000;
+
+
+
+
+    tiket_wrapper.remove('invisible');
+    }
 })
 
 const empty =document.getElementById('canc');
@@ -38,4 +67,8 @@ empty.addEventListener('click', function(){
     full_name.value = "";
     kilometers.value = "";
     age.value = "";
+
+    tiket_wrapper.add('invisible');
+
+    
 })
